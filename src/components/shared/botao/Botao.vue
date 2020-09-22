33f9f11c2f5d7bdaff57891138c89cd02eb75550
thais Lima <thaisdalima@gmail.com>
2020-09-22 09:26:35 -0300

    projeto final

---
+++ src/components/shared/botao/Botao.vue
@@ -0,0 +1,60 @@
+<template>
+    <button @click="disparaAcao()" class="botao":class="estilodoBotao" :type="tipo">{{rotulo}}</button>
+</template>
+<script> 
+export default {
+
+   props: {
+
+       tipo: {
+           required: true,
+           type: String
+       },
+
+       rotulo: {
+
+           required: true,
+           type: String
+       },
+
+       confirmacao: Boolean,
+
+       estilo: String
+   },
+   methods: {
+
+       disparaAcao() {
+
+           if(this.confirmacao) {
+                if(confirm('Confirma operacao?')) {
+                this.$emit('botaoAtivado');
+            }
+
+            return;
+       }
+       this.$emit('botaoAtivado');
+
+           }
+          
+       },
+
+       computed: {
+
+           estilodoBotao () {
+
+               if(this.estilo == 'padrao' || !this.estilo) return 'botao-padrao';
+               if(this.estilo == 'perigo') return 'botao-perigo';
+
+
+           }
+
+       }
+   }
+
+</script>    
+
+<style scoped lang="sass">
+
+    @import './Botao.scss';
+
+</style>
