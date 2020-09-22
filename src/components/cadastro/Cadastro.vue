e8cd3452c0eda82478c27f5387776a22863573a3
thais Lima <thaisdalima@gmail.com>
2020-09-22 09:49:41 -0300

    projeto final

--- src/components/cadastro/Cadastro.vue
+++ src/components/cadastro/Cadastro.vue
@@ -4,8 +4,8 @@
     <h1 class="centralizado">Cadastro</h1>
     <h2 class="centralizado">{{ foto.titulo }}</h2>
 
-    <h2 v-if="foto._id" class="centralizado">Alterando</h2>
-    <h2 v-else class="centralizado">Incluindo</h2>
+    <h2 v-if="foto._id" class="centralizado">Alterando Foto</h2>
+    <h2 v-else class="centralizado">Incluindo Foto</h2>
     
     <form @submit.prevent="grava()">
 
